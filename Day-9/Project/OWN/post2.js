fd9d23fc4b8786953f1e8b2b27b50e31ded7a78e
Leejaw-Chitrakar <leejaw.chitrakar10@gmail.com>
2025-06-28 18:12:30 +0545

    Added post list functionality, patch/put and delete buttons, and corresponding JavaScript functions.

--- Day-9/Project/OWN/post2.js
+++ Day-9/Project/OWN/post2.js
@@ -1,5 +1,5 @@
-// localStorage.setItem('username', 'Raju Shrestha')
-localStorage.clear()
+localStorage.setItem('username', 'Leejaw Chitrakar')
+// localStorage.clear()
 
 const username = localStorage.getItem('username')
 document.getElementById('username').textContent = username || 'Guest'
@@ -24,6 +24,10 @@ function fetchposts() {
                 <strong>Title:</strong> ${post.title}
                 <br>
                 <strong>Body:</strong> ${post.body}
+                <br><br>
+                <button onclick="patchputdata(${post.id})">Patch/Put</button>
+                <button onclick="deletepost(${post.id})">Delete</button>
+                <br><br>
                 `
                 postlist.appendChild(li)
             })
@@ -61,3 +65,28 @@ function addpost() {
     })
 }
 addpost()
+
+function patchputdata(id) {
+    console.log(id)
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        method: `PATCH`,  //PATCH for partial update
+        // method: `PUT`, // PUT for full update
+        // userId:1,
+        // id: id,
+        body: JSON.stringify({
+            title: 'Test Title',
+
+        }),
+    })
+        .then(() => console.log(`Post with ID ${id} updated successfully`))
+        .catch((err) => console.error(err))
+}
+
+function deletepost(id) {
+    console.log(`Post Deleted ID: ${id}`)
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        method: 'DELETE',
+    })
+        .then(() => console.log(`Delete Successful of Id: ${id}`))
+        .catch((err) => console.error(err))
+}
